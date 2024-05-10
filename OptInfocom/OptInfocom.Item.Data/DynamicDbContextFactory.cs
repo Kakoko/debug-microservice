8effa6695b8ee099e2e6ac25ebb488b01442523a
Sambhav <sambhavyadav90@gmail.com>
2024-05-10 15:11:27 +0530

    Done

---
+++ OptInfocom/OptInfocom.Item.Data/DynamicDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptInfocom.Item.Data
+{
+    public class DynamicDbContextFactory
+    {
+        public readonly string _masterConnectionString;
+
+        public DynamicDbContextFactory(IConfiguration configuration)
+        {
+            _masterConnectionString = configuration.GetConnectionString("MyConnectionString");
+        }
+    }
+}
